780724e7094ab21eb5bbf2f03e1eb06ae2daf99c
David Encarnacion <davideg96@outlook.com>
2022-08-29 14:44:44 -0500

    portfolio

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+const option = {
+  arrowParens: 'avoid',
+  singleQuote: true,
+  backetSpacing: true,
+  endOfLine: 'lf',
+  semi: false,
+  tabWidth: 2,
+  trailingComma: 'none'
+}
+
+
+module.exports = option;
